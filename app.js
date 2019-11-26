3472cbd891f72a9a6360d263e3df30943d74dd93
Dasiane Jones <dasianejones@gmail.com>
2019-11-26 11:13:58 -0500

    second commit

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 console.log("Hello from app.js")
+
+// enter-an-amount, deposit, withdraw, 
+
+let balanceDisplay = document.querySelector('.balance');
+
+let amountInput = document.querySelector('.input');
+
+let depositInput = document.querySelector('.deposit');
+
+let withdrawInput = document.querySelector('.withdraw');
+
+console.log(withdrawInput);
