2b06f8054b4935eb2d9ef6f5e7ef7f0a6efa7ba2
Dasiane Jones <dasianejones@gmail.com>
2019-11-26 22:44:38 -0500

    added withdrawal and deposit buttons to savings and checking

--- app.js
+++ app.js
@@ -43,6 +43,14 @@ checkingDepositBtn.addEventListener('click', function(evt) {
     checkingBalance = newBalance;
     UpdateDisplay(checkingBalanceDisplay, checkingBalance);
     });
+
+    checkingWithdrawBtn.addEventListener('click', function(evt) {
+        evt.preventDefault();
+        checkingInput = Number(checkingAmountInput.value);
+        let newBalance = Withdraw(checkingBalance, checkingInput);
+        checkingBalance = newBalance;
+        UpdateDisplay(checkingBalanceDisplay, checkingBalance);
+        });
     
     // line break //
 
@@ -54,6 +62,65 @@ checkingDepositBtn.addEventListener('click', function(evt) {
     console.log('hello');
 
 // line break //
+
+
+
+// Savings Account //
+
+
+// line break //
+
+let savingsDepositBtn = document.querySelector('#savings > .deposit');
+
+let savingsAmountInput = document.querySelector('#savings > .input');
+
+let savingsWithdrawBtn = document.querySelector('#savings > .withdraw');
+
+let savingsBalanceDisplay = document.querySelector('#savings > .balance');
+
+console.log(savingsDepositBtn);
+
+// line break //
+
+// Deposit = (num1, num2) => {
+//     return num1 + num2;
+// };
+
+// Withdraw = (num1, num2) => {
+//     return num1 - num2;
+// };
+
+// line break //
+
+savingsDepositBtn.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    savingsInput = Number(savingsAmountInput.value);
+    let newBalance = Deposit(savingsBalance, savingsInput);
+    savingsBalance = newBalance;
+    UpdateDisplay(savingsBalanceDisplay, savingsBalance);
+    });
+
+savingsWithdrawBtn.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    savingsInput = Number(savingsAmountInput.value);
+    let newBalance = Withdraw(savingsBalance, savingsInput);
+    savingsBalance = newBalance;
+    
+    UpdateDisplay(savingsBalanceDisplay, savingsBalance);
+    });
+    
+    // line break //
+
+    // UpdateDisplay = (display, updatedValue) => {
+    //     return display.innerHTML = updatedValue;
+    // }; 
+
+    console.log(savingsBalance);
+    console.log('hello');
+
+// line break //
+
+
 
 
 
